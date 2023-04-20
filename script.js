c30f4c182202d6551143a6682b9c5cb43600174c
ArdiAbdurrahman <101821904+ArdiAbdurrahman@users.noreply.github.com>
2023-04-20 18:46:52 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// toggle class active
+const navbarNav = document.querySelector(".navbar-nav");
+//ketika humberger menu di klik
+document.querySelector("#humberger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// klik di bagian luar sidebar untuk menghilngkan humberger
+const humberger = document.querySelector("#humberger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!humberger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
